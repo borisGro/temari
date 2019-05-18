02d7897564758009da74726f0f304dde05f63094
theborisgros <theborisgros@penguin>
2019-05-17 21:34:17 -0700

    0.01

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import AddonsNavPage from "./pages/AddonsNavPage";
-import AdvancedNavPage from "./pages/AdvancedNavPage";
-import HomePage from "./pages/HomePage";
+import { homedir } from "os";
+
 
 
 
@@ -10,9 +9,9 @@ class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/addons" component={AddonsNavPage} />
-        <Route exact path="/advanced" component={AdvancedNavPage} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Shop} />
+        <Route exact path="/" component={Contact} />
         <Route
           render={function () {
             return <h1>Not Found</h1>;
